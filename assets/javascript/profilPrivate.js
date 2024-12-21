ab05969340f2c5a3d2a8f98a1d3237237d825092
Kenny Pigny <pigny.kenny@gmail.com>
2024-12-21 01:23:30 +0100

    section ProfilPrivate terminé

--- assets/javascript/profilPrivate.js
+++ assets/javascript/profilPrivate.js
@@ -26,18 +26,45 @@ bannerProfil.addEventListener("click", () => {
 });
 
 uploadBanner.addEventListener("change", (event) => {
-    const file = event.target.files[0];
-  
-    if (file) {
-      const newPhoto = new FileReader();
-  
-      newPhoto.onload = (e) => (bannerProfil.src = e.target.result);
-  
-      newPhoto.readAsDataURL(file);
-    }
-  });
+  const file = event.target.files[0];
 
+  if (file) {
+    const newPhoto = new FileReader();
 
+    newPhoto.onload = (e) => (bannerProfil.src = e.target.result);
+
+    newPhoto.readAsDataURL(file);
+  }
+});
 
 // DROPDOWN DES BOX DE LA PAGE
+const iconDropdownProfil = document.querySelector(".dropdown-general-profil");
+const boxGeneral = document.querySelector(".box-general");
 
+const iconChangePassword = document.querySelector(".dropdown-change-password");
+const boxChangePassword = document.querySelector(".box-change-password");
+
+const iconChangeMail = document.querySelector(".dropdown-change-mail");
+const boxChangeMail = document.querySelector(".box-change-mail");
+
+const iconDelete = document.querySelector(".dropdown-delete-account");
+const boxDelete = document.querySelector(".box-delete");
+
+const iconHistoryProfil = document.querySelector(".dropdown-history-profil");
+const boxHistoryProfil = document.querySelector(".box-history-profil");
+
+iconDropdownProfil.addEventListener("click", () => {
+  boxGeneral.classList.toggle("hide-box");
+});
+iconChangePassword.addEventListener("click", () => {
+    boxChangePassword.classList.toggle("hide-box");
+});
+iconChangeMail.addEventListener("click", () => {
+    boxChangeMail.classList.toggle("hide-box");
+});
+iconDelete.addEventListener("click", () => {
+    boxDelete.classList.toggle("hide-box");
+});
+iconHistoryProfil.addEventListener("click", () => {
+    boxHistoryProfil.classList.toggle("hide-box");
+});
