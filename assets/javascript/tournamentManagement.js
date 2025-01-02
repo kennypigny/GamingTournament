dc99da89452d8a5348b7cb96b3f96f530168a9f0
Kenny Pigny <pigny.kenny@gmail.com>
2025-01-02 10:55:23 +0100

    Ajout des fonction JS dropdown de la page tournamentManagement

--- assets/javascript/tournamentManagement.js
+++ assets/javascript/tournamentManagement.js
@@ -1,9 +1,21 @@
 const dropDownInfo = document.querySelector(".dropdown-info-sheet-tournament");
-const infoGeneral = document.querySelector(".container-info-description")
+const infoGeneral = document.querySelector(".container-info-description");
+
+const dropDownPlayers = document.querySelector(".dropdown-players-tournament");
+const players = document.querySelector(".container-players");
 
 
+const dropDownRegistration = document.querySelector(".dropdown-registration-tournament");
+const registration = document.querySelector(".container-registration");
 
-dropDownInfo.addEventListener('click', () => {
-    infoGeneral.classList.toggle('active-sheet-tournament')
-    
-})
+dropDownInfo.addEventListener("click", () => {
+  infoGeneral.classList.toggle("active-tournament-management");
+});
+
+dropDownPlayers.addEventListener("click", () => {
+  players.classList.toggle("active-tournament-management");
+});
+
+dropDownRegistration.addEventListener("click", () => {
+  registration.classList.toggle("active-tournament-management");
+});
