4dc5b2967bae1fdf123377ba398800ad9592cd2e
Kenny Pigny <pigny.kenny@gmail.com>
2025-01-04 01:31:58 +0100

    Ajout des verifaction regex de la page profilprivate

--- assets/javascript/profilPrivate.js
+++ assets/javascript/profilPrivate.js
@@ -1,4 +1,4 @@
-// SCRIPT CHANGEMENT DE PHOTO ET BANNIERE PROFIL
+//CHANGEMENT DE PHOTO ET BANNIERE PROFIL
 
 const photoProfil = document.getElementById("photo-profil");
 const bannerProfil = document.getElementById("banner-profil");
@@ -68,3 +68,110 @@ iconDelete.addEventListener("click", () => {
 iconHistoryProfil.addEventListener("click", () => {
   boxHistoryProfil.classList.toggle("hide-box");
 });
+
+// VERIFICATION GENERAL
+const onlyLetterRegex = /^[\p{L}]+$/u;
+
+const buttonGeneral = document.getElementById("button-general");
+
+const firstName = document.getElementById("firstname-user");
+
+const secondName = document.getElementById("secondname-user");
+
+const nickName = document.getElementById("nickname-user");
+
+const letterErrorFirstname = document.querySelector(".letter-error-firstname");
+
+const letterErrorSecondname = document.querySelector(
+  ".letter-error-secondname"
+);
+
+const letterErrorNickname = document.querySelector(".letter-error-nickname");
+
+function verificationOnlyLetter(inputGeneral, errorText) {
+  const value = inputGeneral.value;
+
+  if (!onlyLetterRegex.test(value)) {
+    errorText.classList.add("message-error-visible");
+  } else {
+    errorText.classList.remove("message-error-visible");
+
+    // alert("TEST LETTRE VALIDE !");
+  }
+}
+buttonGeneral.addEventListener("click", () =>
+  verificationOnlyLetter(firstName, letterErrorFirstname)
+);
+buttonGeneral.addEventListener("click", () =>
+  verificationOnlyLetter(secondName, letterErrorSecondname)
+);
+buttonGeneral.addEventListener("click", () =>
+  verificationOnlyLetter(nickName, letterErrorNickname)
+);
+
+// VERIFICATION CHANGEMENT DE MOT DE PASSE
+
+const buttonChangePassword = document.getElementById("button-change-password");
+
+function verificationContentSpecialCharacter() {
+  const newPassword = document.getElementById("new-password").value;
+
+  const passwordError = document.querySelector(".password-error");
+
+  const verifyIfSpecialCharacterRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  if (!verifyIfSpecialCharacterRegex.test(newPassword)) {
+    passwordError.classList.add("message-error-visible");
+  } else {
+    passwordError.classList.remove("message-error-visible");
+
+    // alert("TEST MDP VALIDE !");
+  }
+}
+
+function verifySamePassword() {
+  const password = document.getElementById("new-password").value;
+
+  const confirmPassword = document.getElementById("verify-password").value;
+
+  const passwordErrorConfirm = document.querySelector(
+    ".password-error-confirm"
+  );
+
+  if (password !== confirmPassword) {
+    passwordErrorConfirm.classList.add("message-error-visible");
+  } else {
+    passwordErrorConfirm.classList.remove("message-error-visible");
+
+    // alert("TEST C'EST LE MEME MDP");
+  }
+}
+
+buttonChangePassword.addEventListener(
+  "click",
+  verificationContentSpecialCharacter
+);
+
+buttonChangePassword.addEventListener("click", verifySamePassword);
+
+// VERIFICATION CHANGEMENT D EMAIL
+
+const buttonChangeMail = document.getElementById("button-change-mail");
+
+function verifyNewEmail() {
+  const verificationEmailRegex =
+    /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const newEmail = document.getElementById("new-mail").value;
+  const NewEmailError = document.querySelector(".new-email-error");
+
+  if (!verificationEmailRegex.test(newEmail)) {
+    NewEmailError.classList.add("message-error-visible");
+  } else {
+    NewEmailError.classList.remove("message-error-visible");
+
+    alert("EMAIL VALIDE !");
+  }
+}
+
+buttonChangeMail.addEventListener("click", verifyNewEmail);
