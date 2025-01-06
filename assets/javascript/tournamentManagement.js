ddada27b3113603a3993dee82bf96fb22bef08e8
Kenny Pigny <pigny.kenny@gmail.com>
2025-01-06 13:32:27 +0100

    Ajout des fonctionnalité ajouter des joueur et modification de leur pseudo sur la page tournamentManagement

--- assets/javascript/tournamentManagement.js
+++ assets/javascript/tournamentManagement.js
@@ -5,8 +5,9 @@ const infoGeneral = document.querySelector(".container-info-description");
 const dropDownPlayers = document.querySelector(".dropdown-players-tournament");
 const players = document.querySelector(".container-players");
 
-
-const dropDownRegistration = document.querySelector(".dropdown-registration-tournament");
+const dropDownRegistration = document.querySelector(
+  ".dropdown-registration-tournament"
+);
 const registration = document.querySelector(".container-registration");
 
 dropDownInfo.addEventListener("click", () => {
@@ -22,4 +23,82 @@ dropDownRegistration.addEventListener("click", () => {
 });
 
 // AUTO-SELECT-RADIO auto-accept box-registration
-document.getElementById('auto-accept-yes').checked = true;
+document.getElementById("auto-accept-yes").checked = true;
+
+// ADD JOUEUR MANUEL AU TOURNOIS
+const addButton = document.getElementById("add-player");
+const tbody = document.querySelector(".table-players tbody");
+
+
+addButton.addEventListener("click", () => {
+  const playerCount = tbody.querySelectorAll("tr").length + 1;
+
+  const newRowTable = document.createElement("tr");
+
+  // Ajout de la nouvelle ligne sur le tableaux
+  newRowTable.innerHTML = `
+        <td>
+            <label for="list-player${playerCount}" aria-label="Joueur inscrit">
+                <input type="checkbox" name="list-player${playerCount}" id="list-player${playerCount}" />
+            </label>
+        </td>
+        <td>Joueur ${playerCount}</td>
+        <td>
+            <img src="../assets/img/General/crayon.png" alt="Icone modifier" />
+        </td>
+    `;
+
+  tbody.appendChild(newRowTable);
+});
+
+
+// MODIFICATION DU PSEUDO DE LA LISTE DES JOUEURS INSCRIT
+
+tbody.addEventListener("click", function modificationOfNickname(event) {
+  
+  if (event.target.tagName === "IMG") {
+    
+    const rowParent = event.target.closest("tr"); 
+    const nicknameCell = rowParent.querySelector("td:nth-child(2)");
+
+    closeEditWindows();
+
+    const currentNickname = nicknameCell.textContent.trim();
+
+    nicknameCell.innerHTML = `
+      <input type="text" value="${currentNickname}" class="edit-nickname" />
+      <button class="save-btn add">Ok</button>
+    `;
+
+    // Selectionne les nouveaux élements
+    const inputEdit = nicknameCell.querySelector(".edit-nickname");
+    const saveButton = nicknameCell.querySelector(".save-btn");
+    
+    const saveEdition = () => {
+      const newName = inputEdit.value.trim();
+      if (newName) {
+        nicknameCell.textContent = newName;
+      } else {
+        alert("Veuillez ajoutez un pseudo !");
+      }
+    };
+
+    saveButton.addEventListener("click", saveEdition);
+    inputEdit.addEventListener("keypress", (push) => {
+      if (push.key === "Enter") {
+        saveEdition();
+      }
+    });
+  }
+});
+
+// Fonction pour fermer les fenetres d'édition quand une autre s'ouvre
+function closeEditWindows() {
+  const openInputs = tbody.querySelectorAll(".edit-nickname");
+  
+  openInputs.forEach((input) => {
+    const parentCell = input.closest("td");
+    const currentName = input.value.trim();
+    parentCell.textContent = currentName;
+  });
+}
